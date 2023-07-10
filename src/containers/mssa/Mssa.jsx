64ee0644e92e9b74b1a77540a8199d3c89367395
Yous237 <salouhouyousuf@gmail.com>
2023-07-10 14:25:52 -0500

    final

--- src/containers/mssa/Mssa.jsx
+++ src/containers/mssa/Mssa.jsx
@@ -4,28 +4,27 @@ import './mssa.css'
 
 const Mssa = () => {
   return (
-    <div className="painting__irises ">
-        <div className="painting__irises-content">
-        <div className="painting__irises-titles">
-            <h2>Mountain Stream in Summer and Autumn - 19th century</h2>
-            <h4>夏秋渓流図</h4>
-          </div>
-          <p>Born in Kyoto, Ogata Kōrin (1658-1716) was a Japanese painter and landscape illustrator of  one of the major historicals schools of Japanese painting, Rinpa School. Successor of Tawaraya Sōtatsu, Korin is recognized as one of the pillars of the Rinpa school.</p>
-          <p>
-            Pair of 59.4 x 133.4 in six panel screen, Irises was made during the period of luxury in Edo era. The byōbu (wind wall) depicts ultramarine blue Japanese irises in bloom, made from pwodered azurite, repeated in an varying pattern. The background created with gold leaf 
-            blends with ink and colour on paper to give the impression of a river
-          </p>
-          
+    <div className="painting__mssa ">
+        <div className="painting__mssa-content">
+            <div className="painting__mssa-titles">
+                <h2>Mountain Stream in Summer and Autumn - 19th century</h2>
+                <h4>夏秋渓流図</h4>
+            </div>
+            <p>Born in Edo, the former name of Tokyo, Suzuki Kiitsu (1796-1858) was a Japanese painter know for his byobu (wind wall) folding screens. He was a student of Rinpa School and was influenced by the works of his predecessors of which he made numerous representations such as the Wind God and Thunder God by Sōtatsu.</p>
+            <p>
+                Pair of 59.4 x 133.4 in six panel screen, mssa was made during the period of luxury in Edo era. The byōbu (wind wall) depicts ultramarine blue Japanese mssa in bloom, made from pwodered azurite, repeated in an varying pattern. The background created with gold leaf 
+                blends with ink and colour on paper to give the impression of a river
+            </p>
         </div>
-        <div className="painting__irises-container-letters">
-          <div className="painting__irises-japanese-letters">
+        <div className="painting__mssa-container-letters">
+          <div className="painting__mssa-japanese-letters">
           
             <p>鈴</p>
             <p>木</p>
             <p>其</p>
             <p>一</p>
           </div>
-          <div className="painting__irises-letters">
+          <div className="painting__mssa-letters">
               <p>Kiitsu Suzuki</p>
           </div>
         </div>
